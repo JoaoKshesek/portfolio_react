92cf4af750dfacff1ce4cfb9d07b3be7e8232c6d
Joao Kshesek <kshesek.joaovalter@gmail.com>
2022-08-23 17:48:22 -0300

    Add Navbar, Hero section and reset css

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,20 @@
 import './Home.css';
+import astronaut from '/src/assets/images/astronaut.png';
 
 export const Home = () => {
   return (
     <section id="header">
+      <header>
+        <div className="header-text">
+          <h1>Hi! I'm João Valter</h1>
+          <h2>A Physics enthusiast</h2>
+          <p>Graduating in Systems Analysis and Development with successful experience in developing logical
+solutions to complex problems.</p>
+        </div>
+        <div className="header-image">
+          <img src={astronaut} alt="Astronaut reading"/>
+        </div>
+      </header>
     </section>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,6 +3,13 @@ import './Navbar.css';
 export const NavBar = () => {
   return (
     <nav id="navbar">
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#resume">Resume</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
     </nav>
   )
 }
