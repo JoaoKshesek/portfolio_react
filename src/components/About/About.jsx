f1a71d65171d69ff0dd063e74cc148b299a5cda7
Joao Kshesek <kshesek.joaovalter@gmail.com>
2022-08-26 18:43:26 -0300

    Include About Section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,10 @@
 import './About.css';
 import hero from '/src/assets/images/hero.png';
+import github from '/src/assets/images/github.svg';
+import linkedin from '/src/assets/images/linkedin.svg';
+import whatsapp from '/src/assets/images/whatsapp.svg';
+
+
 
 export const About = () => {
   return (
@@ -15,6 +20,19 @@ export const About = () => {
         <p>
           I am a motivated and well-organized person, always looking for a new way to learn.
         </p>
+        <p>
+        At college I studied Python first and then PHP. But it was when I started studying HTML, CSS and JavaScript that I felt programming was what I really wanted to do for my life.
+        </p>
+        <div className='social-media'>
+          <a href="https://github.com/JoaoKshesek" target="blank"><img src={github} alt="" /></a>
+          <a href="https://www.linkedin.com/in/jo%C3%A3o-valter-kshesek-165520213/" target="blank"><img src={linkedin} alt="" /></a>
+          <a href="https://api.whatsapp.com/send?phone=5541995531007" target="blank"><img src={whatsapp} alt="" /></a>
+        </div>
+        <div className='resume'>
+          <a href="#" className='btn'>Download resume EN_US</a>
+          <a href="#" className='btn'>Download resume PT_BR</a>
+
+        </div>
       </div>
     </section>
   )
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,21 @@
+import { useState } from 'react';
 import './Navbar.css';
 
 export const NavBar = () => {
+  const [state, setState] = useState(false)
+
+  const toggle = () => {
+    setState(!state);
+  }
 
   return (
     <section id="navbar">
-      <div className='nav-toggler'>
+      <div className={'nav-toggler ' + (state ? 'open' : '')} onClick={toggle}>
         <span></span>
       </div>
-      <nav id="navigation" className='navigation'>
+      <nav id="navigation" className={'navigation ' + (state ? 'open' : '')} onClick={toggle}>
         <ul>
-          <li><a href="#home">Home</a></li>
+          <li><a href="#home"  className='active'>Home</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#resume">Resume</a></li>
           <li><a href="#projects">Projects</a></li>
