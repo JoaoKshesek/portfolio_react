b710f9ad4e5a5923a963cec85afaf58c38315e59
Joao Kshesek <kshesek.joaovalter@gmail.com>
2022-09-01 23:16:35 -0300

    Start Resume section

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -3,6 +3,17 @@ import './Resume.css';
 export const Resume = () => {
   return (
     <section id="resume">
+      <div className='work'>
+        <div className='col-left'>
+          <h2>Work</h2>
+        </div>
+        <div className='col-right'>
+          <h3>Despachante Valentin Duarte</h3>
+          <p>Administrative Assistant February, 2021 — April, 2022</p>
+          <p>February, 2021 — April, 2022</p>
+          <p>Customer service, focusing on solving customer problems quickly and effectively Elaboration of spreadsheets to control and organize processes Preparation of various documentation, electronic and printed, to be sent to customers, with emphasis on agile work Started to develop a new project for the company's website, using ReactJS as the main technology</p>
+        </div>
+      </div>
     </section>
   )
 }
