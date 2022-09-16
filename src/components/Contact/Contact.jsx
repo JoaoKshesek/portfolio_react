71a6a32a1432de96aded9ea7d8d2577b475e59f0
Joao Kshesek <kshesek.joaovalter@gmail.com>
2022-09-15 23:48:25 -0300

    Add Form Inputs

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,8 +4,14 @@ import './Contact.css';
 export const Contact = () => {
   return (
     <section id="contact">
-
-        <form action=""></form>
+        <h2>Get in Touch</h2>
+        <form action="">
+          <input type="text" placeholder="First Name"/>
+          <input type="text" placeholder="Last Name" />
+          <input type="email" placeholder="Email Address" />
+          <input type="tel" placeholder="Phone Number" />
+          <textarea name="" id="" cols="30" rows="10" placeholder='Message'></textarea>
+        </form>
 
     </section>
   )
