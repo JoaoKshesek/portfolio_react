48d77b24e350a70c60311ff8fb797795b3e8e7d7
Joao Kshesek <kshesek.joaovalter@gmail.com>
2022-10-07 04:05:54 -0300

    Create Button component

---
+++ src/components/Button/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.button`
+    width: 100%;
+    background: ${({ theme }) => theme.COLORS.GRADIENT};
+    color: ${({ theme }) => theme.COLORS.WHITE};
+
+    height: 56px;
+    border: 0;
+    padding: 0 16px;
+    margin-top: 16px;
+    border-radius: 7px;
+    font-weight: 500;
+    font-size: 18px;
+`;
