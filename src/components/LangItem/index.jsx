183e0f44df371553cb11c59076342da0ba007fde
Joao Kshesek <kshesek.joaovalter@gmail.com>
2022-10-06 22:46:27 -0300

    Create LangItem component

---
+++ src/components/LangItem/index.jsx
@@ -0,0 +1,9 @@
+import { Container } from "./styles";
+import { Paragraph } from "../Paragraph";
+export function LangItem({ children }) {
+    return (
+        <Container>
+            {children}
+        </Container>
+    );
+}
