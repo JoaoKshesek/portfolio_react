26a2b68ec6801d0f135c32f57e87e054d8b22243
Joao Kshesek <kshesek.joaovalter@gmail.com>
2022-10-07 04:08:01 -0300

    Create Projects page

---
+++ src/pages/Projects/index.jsx
@@ -0,0 +1,29 @@
+import '../../assets/translations/i18n'
+import { useTranslation } from 'react-i18next';
+
+
+import { Section } from "../../components/Section"
+import { ResumeItem } from "../../components/ResumeItem"
+import { Button } from "../../components/Button"
+
+
+import { Container } from "./styles";
+
+export function Projects(){
+    const {t} = useTranslation();
+    return(
+        <Container>
+            <Section title={t('resume_projects')}>
+                <p>{t('resume_projects_description')}</p>
+                <ResumeItem title={t('resume_project1_title')}>
+                    <p>{t('resume_project1_description')}</p>
+                </ResumeItem>
+                    <a href="https://github.com/JoaoKshesek/portfolio_react/tree/main/src/components/Resume">
+                        <Button title={t('resume_project1_button')} /> 
+                    </a>
+
+            </Section>
+
+        </Container>
+    )
+}
