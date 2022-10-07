c94ddb92e5f80979a7b89fc2f9fc3cfafb6ca454
Joao Kshesek <kshesek.joaovalter@gmail.com>
2022-10-07 04:07:49 -0300

    Create Contact page

---
+++ src/pages/Contact/index.jsx
@@ -0,0 +1,13 @@
+import '../../assets/translations/i18n'
+import { useTranslation } from 'react-i18next';
+
+import { Container } from "./styles";
+
+export function Contact(){
+    const {t} = useTranslation();
+    return(
+        <Container>
+
+        </Container>
+    )
+}
