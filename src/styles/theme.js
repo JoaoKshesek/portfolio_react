8d1b28abbe8f9be0dbce225eb2a89d6324d084d7
Joao Kshesek <kshesek.joaovalter@gmail.com>
2022-10-06 22:47:06 -0300

    Create and set theme setup

---
+++ src/styles/theme.js
@@ -0,0 +1,16 @@
+export default {
+    COLORS: {
+      BACKGROUND_100: "#dcdcdc",
+      BACKGROUND_900: "#101010",
+  
+      GRADIENT: "linear-gradient(95deg, rgba(170, 54, 124, 0.95), rgba(74, 47, 189, 0.95))",
+      ORANGE: "#95b963",
+  
+      GRAY_100: "#a3a3a3",
+      GRAY_200: "#808080",
+      GRAY_300: "#717172",
+      GRAY_500: "#404040",
+  
+      RED: "#FF002E"
+    }
+  }
