005e758017b69795b483ea6b2619b82bd77dcd3e
Joao Kshesek <kshesek.joaovalter@gmail.com>
2022-10-19 21:15:40 -0300

    Update Resume MediaQuery

--- src/components/ResumeItem/styles.js
+++ src/components/ResumeItem/styles.js
@@ -1,15 +1,15 @@
 import styled from 'styled-components';
+import { device } from '../../components/MediaQuery'
+
 
 export const Container = styled.section`
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    margin-top: 30px;
-
-    
-
-    
-    
-    
+    margin-top: 30px; 
+    @media ${device.laptop} { 
+        margin-top: 0;
+        align-items: flex-start;
+    }
  `;
